2ce1c74ef3152abd1de852b2ca82d939109993bb
RonakOnMyPC <kanin.4214@gmail.com>
2015-11-22 22:16:17 +0700

    Add ConfigController.php and Create actionCompany function

---
+++ controllers/ConfigController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\Company;
+use yii\web\Session;
+
+class ConfigController extends Controller {
+
+    public function actionCompany() {
+        $company = Company::find()->one();
+        if (empty($company)) {
+            $company = new Company();
+            $company->vat = 0;
+            $company->logo = '';
+        }
+        $post = Yii::$app->request->post();
+
+        if (!empty($post)) {
+            if (!empty($_FILES['Company']['name']['logo'])) {
+                $tmp_name = $_FILES['Company']['tmp_name']['logo'];
+                $name = $_FILES['Company']['name']['logo'];
+
+                if (file_exists('upload/' . $name)) {
+                    unlink('upload' . $name);
+                }
+            }
+
+            $company->name = $post['Company']['name'];
+            $company->tax_code = $post['Company']['tax_code'];
+            $company->tel = $post['Company']['tel'];
+            $company->website = $post['Company']['website'];
+            $company->address = $post['Company']['address'];
+            $company->vat = $post['Company']['vat'];
+
+            if ($company->save()) {
+                $session = new Session();
+                $session->setFlash('message', 'บันทึกรายการแล้ว');
+
+                return $this->redirect(['company']);
+            }
+        }
+        return $this->render('//config/company', [
+                    'company' => $company
+        ]);
+    }
+
+}
