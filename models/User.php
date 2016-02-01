df91d77735429beddd1c289456d1d362817469e6
RonakOnMyPC <kanin.4214@gmail.com>
2016-02-01 22:01:17 +0700

    add rules, attributelabels, getUserType and getBranch function

--- models/User.php
+++ models/User.php
@@ -10,4 +10,32 @@ class User extends ActiveRecord {
         return 'user';
     }
 
+    public function rules() {
+        return [
+            [['fname', 'lname', 'usr', 'pwd', 'tel', 'email'], 'required']
+        ];
+    }
+
+    public function attributeLabels() {
+        return [
+            'user_type_id' => 'กลุ่มผู้ใช้',
+            'fname' => 'ชื่อจริง',
+            'lname' => 'นามสกุล',
+            'branch_id' => 'สาขา',
+            'usr' => 'username',
+            'pwd' => 'password',
+            'tel' => 'เบอร์โทร',
+            'email' => 'อีเมล์',
+            'status' => 'สถานะ'
+        ];
+    }
+
+    public function getUserType() {
+        return $this->hasOne(UserType::className(), ['id' => 'user_type_id']);
+    }
+
+    public function getBranch() {
+        return $this->hasOne(Branch::className(), ['id' => 'branch_id']);
+    }
+
 }
