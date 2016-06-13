2842b90cd106ea8cb5f7a5895fc5b19af28577d6
Kanin Ouchin <Kanin Ouchin>
2016-06-13 09:35:26 +0700

    เพิ่ม file

---
+++ views/product_type/index.php
@@ -0,0 +1,9 @@
+<div class="panel panel-info" style="margin-right: 20px;">
+    <div class="panel-heading">
+        <span class="glyphicon glyphicon-book"></span>
+        ประเภทสินค้า
+    </div>
+    <div class="panel-body">
+        
+    </div>
+</div>
